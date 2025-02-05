c75f2cf0e50ba2611a63a89080b28ae28110ce0c
Sanjeev Chauhan <schauh81@my.centennialcollege.ca>
2025-02-05 00:09:32 -0500

    Updated player controller (#2)

--- Escape Sync/Assets/_Project/Scripts/PlayerController.cs
+++ Escape Sync/Assets/_Project/Scripts/PlayerController.cs
@@ -1,68 +1,173 @@
-using UnityEngine;
+using System; // Importing the System namespace for general utilities.
+using UnityEngine; // Importing UnityEngine for core Unity functionality.
+using UnityEngine.InputSystem; // Importing UnityEngine.InputSystem for handling player input.
 
-namespace COMP305
+namespace COMP305 // Defining a namespace to organize the code within the COMP305 project.
 {
-    [RequireComponent(typeof(Rigidbody2D))] // If the GameObject doesn't already have a Rigidbody2D, Unity will automatically add it when the script is added.
-    [RequireComponent(typeof(BoxCollider2D))] // If the GameObject doesn't already have a BoxCollider2D, Unity will automatically add it when the script is added.
+    // Ensures that this GameObject has a Rigidbody2D and BoxCollider2D component attached.
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(BoxCollider2D))]
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private InputReader input;
-        [SerializeField] private Rigidbody2D rb;
-        [SerializeField] private BoxCollider2D bc;
-        [SerializeField] private Vector2 movement;
+        [Header("Movement Values")]
+        [SerializeField] private float speed = 5.0f; // Speed of the player's movement
+        [SerializeField] private float jumpForce = 3.0f; // Strength of the player's jump
 
-        [SerializeField] private float moveSpeed = 200f;
+        private Rigidbody2D rb;
+        private BoxCollider2D boxCollider;
+        private Vector2 moveInput; // Stores the player's movement input values.
+        private bool isJumping = false; // Flag to check if the player is jumping.
 
-        [SerializeField] private Transform mainCam;
+        private InputSystem_Actions input; // Reference to the input system actions.
+        public string playerType; // Stores whether the player is "Player1" or "Player2".
+
+        private Animator anim;
+        private bool isGrounded; // Flag to check if the player is on ground.
+        private bool canDoubleJump; // Flag to check if the player can double jump.
 
         private void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
-            bc = GetComponent<BoxCollider2D>();
-            mainCam = Camera.main.transform;
-        }
-
-        private void Start()
-        {
-            //Debug.Log("Start");
-            input.EnablePlayerActions(); // Enables the input to be used for thr player controller
+            boxCollider = GetComponent<BoxCollider2D>();
+            anim = GetComponent<Animator>();
+            input = new InputSystem_Actions();
         }
 
         private void OnEnable()
         {
-            // Subscribes to the 'Move' event. 
-            // When the 'Move' event is triggered, the 'GetMovement' method will be called.
-            input.Move += GetMovement;
+            // Setting up controls for Player1
+            if (playerType == "Player1")
+            {
+                input.Player1.Move.performed += OnMove; // Registers OnMove function for movement input.
+                input.Player1.Move.canceled += OnMove; // Registers OnMove function when movement stops.
+                input.Player1.Jump.performed += OnJump; // Registers OnJump function for jump input.
+                input.Player1.Interact.performed += OnInteract; // Registers OnInteract function for interaction.
+                input.Player1.Enable(); // Enables input actions for Player1.
+            }
+
+            // Setting up controls for Player2
+            if (playerType == "Player2")
+            {
+                input.Player2.Move.performed += OnMove;
+                input.Player2.Move.canceled += OnMove;
+                input.Player2.Jump.performed += OnJump;
+                input.Player2.Interact.performed += OnInteract;
+                input.Player2.Enable(); // Enables input actions for Player2.
+            }
         }
 
         private void OnDisable()
         {
-            // Unsubscribes from the 'Move' event.
-            // This ensures that when the 'Move' event is triggered, the 'GetMovement' method will no longer be called.
-            input.Move -= GetMovement;
+            // Removing event listeners for Player1
+            if (playerType == "Player1")
+            {
+                input.Player1.Move.performed -= OnMove;
+                input.Player1.Move.canceled -= OnMove;
+                input.Player1.Jump.performed -= OnJump;
+                input.Player1.Interact.performed -= OnInteract;
+                input.Player1.Disable(); // Disable input actions for Player2.
+            }
+
+            // Removing event listeners for Player2
+            if (playerType == "Player2")
+            {
+                input.Player2.Move.performed -= OnMove;
+                input.Player2.Move.canceled -= OnMove;
+                input.Player2.Jump.performed -= OnJump;
+                input.Player2.Interact.performed -= OnInteract;
+                input.Player2.Disable(); // Disable input actions for Player2.
+            }
         }
 
         private void FixedUpdate()
         {
-            UpdateMovement();
+            // Move the player horizontally based on input
+            rb.linearVelocity = new Vector2(moveInput.x * speed, rb.linearVelocity.y);
+
+            // Handle jump logic
+            if (isJumping)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                isJumping = false; // Reset jump flag after jumping.
+            }
+
+            // Update animator states
+            anim.SetBool("IsGrounded", isGrounded);
+            anim.SetBool("IsFalling", !isGrounded && rb.linearVelocity.y < -0.1f);
+
+            // Ensure idle state when the player is grounded and not moving
+            if (isGrounded && Mathf.Approximately(rb.linearVelocity.x, 0))
+            {
+                anim.SetBool("IsIdle", true);
+                anim.SetBool("IsRunning", false);
+            }
+            else
+            {
+                anim.SetBool("IsIdle", false);
+            }
         }
 
-        private void UpdateMovement()
+        private void OnMove(InputAction.CallbackContext context)
         {
+            moveInput = new Vector2(context.ReadValue<float>(), 0); // Read horizontal movement input.
 
+            if (moveInput.x != 0)
+            {
+                anim.SetBool("IsRunning", true);
+
+                // Flip player direction based on movement
+                float originalScaleX = Mathf.Abs(transform.localScale.x);
+                transform.localScale = new Vector3(originalScaleX * Mathf.Sign(moveInput.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                anim.SetBool("IsRunning", false);
+                anim.SetBool("IsIdle", true); // Set idle animation if player stops moving.
+            }
         }
 
-        private void HandleMovement()
+        private void OnJump(InputAction.CallbackContext context)
         {
-            var velocity = moveSpeed * Time.fixedDeltaTime;
+            if (context.performed && isGrounded)
+            {
+                isJumping = true;
+                isGrounded = false;
+                canDoubleJump = true;
+                anim.SetBool("IsJumping", true);
+            }
+            else if (context.performed && canDoubleJump)
+            {
+                isJumping = true;
+                canDoubleJump = false;
+                anim.SetTrigger("DoubleJump");
+            }
         }
 
-        private void GetMovement(Vector2 move)
+        private void OnInteract(InputAction.CallbackContext context)
         {
-            //Debug.Log("Input Working" + move);
-            movement.x = move.x;
-            //movement.y = move.y;
+            if (context.performed)
+            {
+                Debug.Log(playerType + " Interacted!"); // Logs interaction to the console.
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Ground")) // Check if colliding with the ground.
+            {
+                isGrounded = true;
+                anim.SetBool("IsJumping", false);
+                anim.SetBool("IsDoubleJump", false);
+                anim.SetBool("IsFalling", false);
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Ground")) // Detect when player leaves the ground.
+            {
+                isGrounded = false;
+            }
         }
     }
-
 }
