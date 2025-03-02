8b09b1260bc6f46ecd804b4ed63624c618051125
sanjeev-cs <sanjeevchauhan3090@gmail.com>
2025-03-01 23:01:13 -0500

    Created a new test level with parallax effect, and also implemented a camera controller to follow the player

--- Escape Sync/Assets/_Project/Scripts/CameraFollow.cs
+++ Escape Sync/Assets/_Project/Scripts/CameraFollow.cs
@@ -1,17 +1,42 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace COMP305
 {
     public class CameraFollow : MonoBehaviour
     {
-        [SerializeField] private float followSpeed = 2.0f;
+        [Header("Camera Variables")]
         [SerializeField] private Transform target;
-        [SerializeField] private float yOffset = 1f;
+        //[SerializeField] private List<Transform> target = new List<Transform>();
+        [SerializeField] private Vector3 Offset;
+        [Range(1, 10)]
+        [SerializeField] private float smoothFactor;
 
-        private void Update()
+        [Header("Camera Bounds")]
+        [SerializeField] private Vector3 minValue;
+        [SerializeField] private Vector3 maxValue;
+
+
+        private void FixedUpdate()
         {
-            Vector3 newPosition = new Vector3(target.position.x, target.position.y + yOffset, -10.0f);
-            transform.position = Vector3.Slerp(transform.position, newPosition, followSpeed * Time.deltaTime);
+            Follow();
+        }
+        private void Follow()
+        {
+            Vector3 targetPosition = target.position + Offset;
+
+            // Define minimum x,y,z values and maximum x,y,z values
+            // Verify if the targetPosiiton is out of the bound or not
+            //Limit it to minimum and maximum values
+            Vector3 boundPosition = new Vector3(
+                Mathf.Clamp(targetPosition.x, minValue.x, maxValue.x),
+                Mathf.Clamp(targetPosition.y, minValue.y, maxValue.y),
+                Mathf.Clamp(targetPosition.z, minValue.z, maxValue.z)
+                );
+
+            Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor * Time.deltaTime);
+            transform.position = smoothPosition;
         }
     }
 }
--- Escape Sync/Assets/_Project/Scripts/ParallaxBackground.cs
+++ Escape Sync/Assets/_Project/Scripts/ParallaxBackground.cs
@@ -7,11 +7,15 @@ namespace COMP305
         [SerializeField] private Vector2 parallaxEffectMultiplier;
         private Transform cameraTrasform;
         private Vector3 lastCameraPosition;
+        private float textureUnitSizeX;
 
         private void Start()
         {
             cameraTrasform = Camera.main.transform;
             lastCameraPosition = cameraTrasform.position;
+            Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+            Texture2D texture = sprite.texture;
+            textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
         }
 
         private void LateUpdate()
@@ -19,6 +23,12 @@ namespace COMP305
             Vector3 deltaMovement = cameraTrasform.position - lastCameraPosition;
             transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
             lastCameraPosition = cameraTrasform.position;
+
+            //if (Mathf.Abs(cameraTrasform.position.x - transform.position.x) >= textureUnitSizeX)
+            //{
+            //    float offsetPositionX = (cameraTrasform.position.x - transform.position.x) % textureUnitSizeX;
+            //    transform.position = new Vector3(cameraTrasform.position.x + offsetPositionX, transform.position.y);
+            //}
         }
     }
 }
