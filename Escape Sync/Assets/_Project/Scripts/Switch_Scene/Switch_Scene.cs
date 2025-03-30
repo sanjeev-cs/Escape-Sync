e8aceb0e99cf85e6e2204f370e0e45f57118b925
JingfuLuo437 <Lluojingfu@gmail.com>
2025-03-31 00:35:16 +0800

    Add_Restart_Exit

--- Escape Sync/Assets/_Project/Scripts/Switch_Scene/Switch_Scene.cs
+++ Escape Sync/Assets/_Project/Scripts/Switch_Scene/Switch_Scene.cs
@@ -7,13 +7,24 @@ namespace COMP305
     {
         public void Switch(){
             Debug.Log("Start Game Button Clicked");
-            SceneManager.LoadScene("Level-1");
+            SceneManager.LoadScene("Level-1",LoadSceneMode.Single);
     }
         public void Run() {
             Debug.Log("Start Game Button Clicked");
         }
 
-    
+        public void QuitApplication()
+        {
+            // 在编辑器中打印消息以确认调用
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // 在构建的应用程序中退出游戏
+        Application.Quit();
+#endif
+        }
+
+
 
     }
 }
